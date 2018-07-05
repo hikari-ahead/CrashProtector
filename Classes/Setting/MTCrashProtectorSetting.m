1614c322b56d548c10c87e50dacf9425b4b04e48
kuang jeon <pt7ace@gmail.com>
2018-07-05 15:02:50 +0800

    [Bugfix]settingDic可从bundle中寻找并copy到指定目录

--- Classes/Setting/MTCrashProtectorSetting.m
+++ Classes/Setting/MTCrashProtectorSetting.m
@@ -76,22 +76,37 @@ static MTCrashProtectorSetting *instance;
 
 - (NSMutableDictionary *)settingDic {
     if (!_settingDic) {
+        // Document目录
         NSString *path = [self plistPath];
         BOOL plistExists = [NSFileManager.defaultManager fileExistsAtPath:path];
         if (!plistExists) {
-            _settingDic = [NSMutableDictionary dictionaryWithDictionary:@{
-                                                                          @"Module": @{
-                                                                                  @"Container":@YES,
-                                                                                  @"NSTimer":@YES,
-                                                                                  @"Notification":@YES,
-                                                                                  @"Observer":@YES,
-                                                                                  @"Selector":@YES
-                                                                                  },
-                                                                          @"enable": @YES,
-                                                                          @"maxOSVersion": @"11.99",
-                                                                          @"minOSVersion": @"9.0"
-                                                                          }];
-            [_settingDic writeToFile:path atomically:YES];
+            // mainBundle是否存在手动配置的
+            NSString *bPath = [[NSBundle mainBundle] pathForResource:@"MTCrashProtectorSetting" ofType:@"plist"];
+            NSError *copyError = nil;
+            if (bPath) {
+                // copy到document中
+                [NSFileManager.defaultManager copyItemAtPath:bPath toPath:path error:&copyError];
+                if (!copyError) {
+                    _settingDic = [NSMutableDictionary dictionaryWithContentsOfFile:path];
+                    return _settingDic;
+                }
+            }
+            // copy失败，并且bundle中不存在
+            if (copyError || !bPath) {
+                _settingDic = [NSMutableDictionary dictionaryWithDictionary:@{
+                                                                              @"Module": @{
+                                                                                      @"Container":@YES,
+                                                                                      @"NSTimer":@YES,
+                                                                                      @"Notification":@YES,
+                                                                                      @"Observer":@YES,
+                                                                                      @"Selector":@YES
+                                                                                      },
+                                                                              @"enable": @YES,
+                                                                              @"maxOSVersion": @"11.99",
+                                                                              @"minOSVersion": @"9.0"
+                                                                              }];
+                [_settingDic writeToFile:path atomically:YES];
+            }
             return _settingDic;
         }else {
             _settingDic = [NSMutableDictionary dictionaryWithContentsOfFile:path];
